f7fe27a06922cc9ba5ba06ba731f8f46a607f3af
Siddiq06 <150819688+Siddiq06@users.noreply.github.com>
2026-05-04 20:33:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Internship from "./components/Internship";
+import Education from "./components/Education";
 import "./App.css";
 
 function App(){
